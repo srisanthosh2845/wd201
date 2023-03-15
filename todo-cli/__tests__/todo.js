6f9e8bef5141f7fcf43d6e220de8dd664da47bf2
sri santhosh <125262905+srisanthosh2845@users.noreply.github.com>
2023-03-15 19:40:24 +0530

    first commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 const todoList = require("../todo");
-const { add, markAsComplete, overdue, dueToday, dueLater } = todoList();
-var d = new Date();
+
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
+
 const formattedDate = (d) => {
   return d.toISOString().split("T")[0];
 };
+
 var dateToday = new Date();
 const today = formattedDate(dateToday);
 const yesterday = formattedDate(
@@ -12,38 +17,73 @@ const yesterday = formattedDate(
 const tomorrow = formattedDate(
   new Date(new Date().setDate(dateToday.getDate() + 1))
 );
-describe("TodoList Test Suite", () => {
+
+describe("Todolist Test Suite", () => {
   beforeAll(() => {
     add({
-      title: "Task Todo",
+      title: "test todo",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date().toISOString().slice(0, 10),
     });
-  });
-  test("add new todo", () => {
-    const todoItemsCount = all.length;
-    add({
-      title: "Submit assignment",
-      dueDate: yesterday,
-      completed: false,
-    });
-    expect(all.length).toBe(todoItemsCount + 1);
   });
 
-  test("mark as complete", () => {
+  test("Should add new todo", () => {
+    const todoItensCount = all.length;
+    add({
+      title: "test todo",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(all.length).toBe(todoItensCount + 1);
+  });
+
+  test("todo as completed", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test("overdue", () => {
-    expect(overdue().length).toBe(1);
+
+  test("overdue items", () => {
+    add({
+      title: "submit assignment",
+      completed: false,
+      dueDate: yesterday,
+    });
+    add({
+      title: "test 1",
+      completed: false,
+      dueDate: yesterday,
+    });
+    expect(overdue().length).toBe(2);
   });
-  test("dueToday", () => {
-    add({ title: "Pay many", dueDate: today, completed: true });
-    expect(dueToday().length).toBe(1);
+
+  test("due today", () => {
+    add({
+      title: "rent rent",
+      completed: true,
+      dueDate: today,
+    });
+    add({
+      title: "service vehicle",
+      completed: false,
+      dueDate: today,
+    });
+
+    add({
+      title: "pay exam fees",
+      completed: true,
+      dueDate: today,
+    });
+    expect(dueToday().length).toBe(5);
   });
-  test("dueLater data", () => {
-    add({ title: "goto school", dueDate: tomorrow, completed: false });
+
+  test("due later", () => {
+    add({
+      title: "pay exam fees",
+      completed: false,
+      dueDate: tomorrow,
+    });
+
     expect(dueLater().length).toBe(1);
   });
 });
